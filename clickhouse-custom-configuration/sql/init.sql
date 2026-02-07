a3e7cf8f9d50d913147da3f6d20d8c689bf473fb
517ANT39 <toni.tashchilin@inbox.ru>
2026-02-07 20:46:17 +0400

    [feat]: Написал набор таблиц для реализации интеграции clickhouse и kafka, добавил clickhouse в docker-compose.yaml

---
+++ clickhouse-custom-configuration/sql/init.sql
@@ -0,0 +1,53 @@
+
+
+create table info_processes_running_in_servers (
+   username String,
+   cmdline Array(String),
+   memory_info Tuple(UInt32, UInt32, UInt32, UInt32, UInt32, UInt32, UInt32), -- По порядку поля памяти
+   environ Map(String, String),
+   name String,
+   cpu_percent Float32,
+   open_files Nested(
+       path String,
+       fd UInt32,
+       inode UInt32,
+       mode Char,
+       size UInt32
+   ),
+   pid UInt32,
+   connections Array(String),
+   io_counters Tuple(UInt32, UInt32, UInt32, UInt32, UInt32, UInt32), -- IO статистики
+   host_id String
+) ENGINE = Kafka
+  SETTINGS kafka_broker_list = 'kafka:9092',
+          kafka_topic_list = 'processes_info',
+          kafka_group_name = 'info_processes_running_in_servers_consumer',
+          kafka_format = 'JSONEachRow',
+          kafka_num_consumers = 1;
+
+CREATE TABLE data_info_processes_running_in_servers (
+    username String,
+    cmdline Array(String),
+    memory_info Tuple(UInt32, UInt32, UInt32, UInt32, UInt32, UInt32, UInt32), -- По порядку поля памяти
+    environ Map(String, String),
+    name String,
+    cpu_percent Float32,
+    open_files Nested(
+       path String,
+       fd UInt32,
+       inode UInt32,
+       mode Char,
+       size UInt32
+    ),
+    pid UInt32,
+    connections Array(String),
+    io_counters Tuple(UInt32, UInt32, UInt32, UInt32, UInt32, UInt32), -- IO статистики
+    host_id String
+    ) ENGINE = MergeTree
+ORDER BY (host_id);
+
+create MATERIALIZED VIEW consume_info_processes_running_in_servers TO data_info_processes_running_in_servers
+AS
+SELECT
+    *
+FROM info_processes_running_in_servers;
